f18d550801991974e52bce24fde807d0e6afdac0
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-09-02 18:30:04 -0300

    Tela de produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: 'produto', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'produto', component: ProdutoComponent}
 ];
 
 @NgModule({
--- src/app/components/card-produto/card-produto.component.ts
+++ src/app/components/card-produto/card-produto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-produto',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardProdutoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+  }
+
+  abrirProduto(){
+    this.router.navigate(['/produto'])
   }
 
 }

fbca8626e5607f246eb3a2a1b19d4c6df63ddf60
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-09-06 16:15:10 -0300

    Botao flutante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { CardProdutoComponent } from './components/card-produto/card-produto.component';
 import { BtnNomeListaComponent } from './components/home/btn-nome-lista/btn-nome-lista.component';
 import { ItemCompraComponent } from './components/item-compra/item-compra.component';
+import { BtnNovoItemComponent } from './components/home/btn-novo-item/btn-novo-item.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { ItemCompraComponent } from './components/item-compra/item-compra.compon
     CardProdutoComponent,
     BtnNomeListaComponent,
     ItemCompraComponent,
+    BtnNovoItemComponent,
   ],
   imports: [
     BrowserModule,

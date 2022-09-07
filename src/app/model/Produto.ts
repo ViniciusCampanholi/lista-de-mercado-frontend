492753a4dd996be47f8b9a3a84b274c35bd61d24
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-09-06 23:55:19 -0300

    Melhorias no tamanho das fontes

---
+++ src/app/model/Produto.ts
@@ -0,0 +1,9 @@
+import { Marca } from "./Marca"
+
+export class Produto{
+  public id!: number
+  public nome!: string
+  public descricao!: string
+  public preco!: number
+  public marca!: Marca
+}
